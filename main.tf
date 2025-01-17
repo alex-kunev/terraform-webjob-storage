adac6f893cdfb217b1eb0dc0e4254dd7392e96df
alex-kunev <69144667+alex-kunev@users.noreply.github.com>
2025-01-17 22:45:07 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-webapp"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  app_settings = {
+    "AzureStorageStr" = azurerm_storage_account.example.primary_connection_string
+  }
+}
+
+output "storage_account_connection_string" {
+  value = azurerm_storage_account.example.primary_connection_string
